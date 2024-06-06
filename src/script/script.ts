7b69f5bb76d9fc160d4e1337eb7b7fee65848a33
derickferreira <derickferreira036@gmail.com>
2024-06-06 08:28:16 -0300

    Taking a break now. Two files have been modified, the script.ts file was compiled, thus generating the script.js which is rendered alongside the browser. I've added the getLetterLowerCase functon and I'm using ASCII as the basis for data retrieval.

--- src/script/script.ts
+++ src/script/script.ts
@@ -1,13 +1,19 @@
 "use strict";
 // select elements
-const cursorDot = document.querySelector("[data-cursor-dot]") as HTMLDivElement;
+const cursorDot: HTMLDivElement | null =
+  document.querySelector("[data-cursor-dot]");
 
 // functions
+const getLtterLowerCase = (): String => {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+};
 
 // events
-window.addEventListener("mousemove", (event) => {
-  const posX = event.clientX;
-  const posY = event.clientY;
-  cursorDot.style.left = `${posX}px`;
-  cursorDot.style.top = `${posY}px`;
+window.addEventListener("mousemove", (event: MouseEvent) => {
+  if (cursorDot) {
+    const posX = event.clientX;
+    const posY = event.clientY;
+    cursorDot.style.left = `${posX}px`;
+    cursorDot.style.top = `${posY}px`;
+  }
 });
